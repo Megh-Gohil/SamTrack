464ce982eacea1a17a9a5e9db852b0b6b94bee38
Megh Gohil <96693481+Megh-Gohil@users.noreply.github.com>
2025-02-14 00:04:53 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -15,13 +15,14 @@ pip install numpy opencv-python pycocotools matplotlib Pillow scikit-image
 pip install gradio==3.39.0 zip gdown ffmpeg==1.4
 pip install timm==0.4.5
 pip install wget
+pip install spatial-correlation-sampler
 
-# Install Pytorch Correlation
-git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
-cd Pytorch-Correlation-extension
-python setup.py install
-cd -
-cp -r Pytorch-Correlation-extension/Correlation_Module/spatial_correlation_sampler .
+# # Install Pytorch Correlation
+# git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
+# cd Pytorch-Correlation-extension
+# python setup.py install
+# cd -
+# cp -r Pytorch-Correlation-extension/Correlation_Module/spatial_correlation_sampler .
 
 # Install AST
 git clone https://github.com/YuanGongND/ast.git ast_master
