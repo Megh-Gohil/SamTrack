9d5772bc3ec6089452c1fec3556d41ef62c05190
Megh Gohil <96693481+Megh-Gohil@users.noreply.github.com>
2025-02-13 23:00:01 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -3,7 +3,11 @@ cd sam; pip install -e .
 cd -
 
 # Install Grounding-Dino
-pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
+# pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
+git clone https://github.com/IDEA-Research/GroundingDINO.git
+cd groundingdino
+pip install -e .
+cd -
 
 # Install other lib
 pip install numpy opencv-python pycocotools matplotlib Pillow scikit-image
